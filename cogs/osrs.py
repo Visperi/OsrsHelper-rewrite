a7d86b05f07cada492b7644b20d534814b8c7f31
Visperi <niksuposti@gmail.com>
2019-02-14 01:10:02 +0200

    Add license

--- cogs/osrs.py
+++ cogs/osrs.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2019 Visperi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import aiohttp
 import math
 from discord.ext import commands
@@ -63,8 +87,7 @@ async def make_scoretable(highscores_data: list, username: str, gains: bool = Fa
     skillnames = ["Total", "Attack", "Defence", "Strength", "Hitpoints", "Ranged", "Prayer", "Magic",
                   "Cooking", "Woodcutting", "Fletching", "Fishing", "Firemaking", "Crafting", "Smithing",
                   "Mining", "Herblore", "Agility", "Thieving", "Slayer", "Farming", "Runecrafting",
-                  "Hunter",
-                  "Construction"]
+                  "Hunter", "Construction"]
     cluenames = ["All", "Easy", "Medium", "Hard", "Elite", "Master"]
     skills = highscores_data[:24]
     clues = highscores_data[26:32]
